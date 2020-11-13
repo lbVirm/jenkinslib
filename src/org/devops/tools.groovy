69f21728f15b5a2b141ca7a35e0f349310e6be27
lbVirm <45552084+lbVirm@users.noreply.github.com>
2020-11-13 15:36:11 +0800

    更新

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -9,7 +9,7 @@ class tools implements Serializable {
     }
     
     //格式化输出
-    def PrintMes(value,color){
+    void PrintMes(value,color){
         colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
                   'blue'  : "\033[47;34m ${value} \033[0m",
                   'green' : "[1;32m>>>>>>>>>>${value}>>>>>>>>>>[m",
